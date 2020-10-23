fe0feb3e17eb5f7915387f6ff2ba968c95810392
uedayusuke <uedayusuke1107@gmail.com>
2020-10-23 12:25:52 +0900

    [ADD]chap1:ソースファイルの作り方

--- chpa1/1/Sample.java
+++ chpa1/1/Sample.java
@@ -1,5 +1,13 @@
+// クラスを宣言→ソースファイル内には一つ以上のクラスが必須
 class Sample {
+	/*
+	mainメソッド→特別なメソッド 実行開始位置を表し、必ず最初に実行される
+	*/
 	public static void main(String[] args) {
+		/*
+		System.out.println→何かしら実行結果を表示
+		print(出力)+line(行)
+		*/
 		System.out.println("Hello");
 	}
 }
