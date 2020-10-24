fe466789addf35ad32f6584ffabec81b89869ed5
uedayusuke <uedayusuke1107@gmail.com>
2020-10-24 19:33:54 +0900

    [ADD]chap1:パッケージ化

--- chap1/7/com/se/Main.java
+++ chap1/7/com/se/Main.java
@@ -1,9 +1,13 @@
 package com.se;
+
+// 利用したいクラス名をimport文を用いて、パッケージを含めて記述
 import com.se.ren.Foo;
 
 public class Main {
 	public static void main(String[] args) {
+		// Fooクラスを初期化し"f"に代入
 		Foo f = new Foo();
+		// f(Fooクラス)を出力
 		f.print();
 	}
 }
--- chap1/7/com/se/ren/Foo.java
+++ chap1/7/com/se/ren/Foo.java
@@ -1,5 +1,6 @@
 package com.se.ren;
 
+// 自身が属するパッケージ以外の参照を許可する場合は"public"が必要
 public class Foo {
 	public void print() {
 		System.out.println("Foo#print()");
