b8cce93b9ccba4d1bccb28c298084a1c247de64b
uedayusuke <uedayusuke1107@gmail.com>
2020-10-25 16:11:10 +0900

    [ADD]chap2:コメント

--- chap2/3/Main.java
+++ chap2/3/Main.java
@@ -1,14 +1,16 @@
 public class Main {
 	public static void main(String[] args) {
+		// 変数を宣言し初期値を代入
 		byte b = 10;
 		short s = 20;
 		int i = 30;
-		long l = 40L;
-		float f = 1.15F;
+		long l = 40L; // long型の変数にlong値を代入するときは"L"もしくは"l"を付加
+		float f = 1.15F; // float型の変数にfloat値を代入するときは"F"もしくは"f"を付加
 		double d = 5.78;
 		char c = 'A';
 		boolean o = true;
 
+		// 出力
 		System.out.println("byte	:" + b);
 		System.out.println("short	:" + s);
 		System.out.println("int	:" + i);
