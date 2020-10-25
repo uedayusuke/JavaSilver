3ffb7b40e7fd6fc152f90719680620cb26c0e4f3
uedayusuke <uedayusuke1107@gmail.com>
2020-10-25 14:38:34 +0900

    [ADD]chap2:アンダースコア

---
+++ chap2/2/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		float x1 = 3_.1415F;
+		float x2 = 3._1415F;
+		long x3 = 999_99_9999_L;
+		int x4 = _52;
+		int x5 = 5_2;
+		int x6 = 52_;
+		int x7 = 5_____2;
+		int x8 = 0_x52;
+		int x9 = 0x_52;
+		int x10 = 0x5_2;
+		int x11 = 0_52;
+	}
+}
