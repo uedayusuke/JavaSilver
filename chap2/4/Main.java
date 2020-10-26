c12f9af1ff4401eb92109fcd038949aa4a30ce90
uedayusuke <uedayusuke1107@gmail.com>
2020-10-26 13:27:07 +0900

    [ADD]chap2:符号付き整数と浮動小数点

--- chap2/4/Main.java
+++ chap2/4/Main.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
     int num1 = 10;
     long num2  = 30;
+
     /*
     以下の理由でコンパイルエラーを起こす
     ↓ 整数のリテラルはデフォルトでまずint型で認識されるため、桁数が大きい場合扱えない。その為、long型の変数に代入する前に末尾にL(もしくはl)をつける必要がある
@@ -10,5 +11,8 @@ public class Main {
     ↓ 浮動小数点のリテラルはデフォルトでdouble型として認識される為、float型の変数に代入する前に末尾にF(もしくはf)をつける必要がある
     float num4 = 10.0;
     */
+
+    long num3 = 10000000000L;
+    float num4 = 10.0F;
   }
 }
