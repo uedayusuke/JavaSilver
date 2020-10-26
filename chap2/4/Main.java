1d9c113193f7fea3a99aa9e3cb66414461bb942e
uedayusuke <uedayusuke1107@gmail.com>
2020-10-26 13:14:08 +0900

    [ADD]chap2:符号付き整数と浮動小数点

--- chap2/4/Main.java
+++ chap2/4/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-	public static void main(String[] args) {
-		int num1 = 10;
-		long num2 = 30;
-		long num3 = 10000000000;
-		float num4 = 10.0;
-	}
+  public static void main(String[] args) {
+    int num1 = 10;
+    long num2  = 30;
+    //long num3 = 10000000000;  // ƒRƒ“ƒpƒCƒ‹ƒGƒ‰[
+    //float num4 = 10.0;        // ƒRƒ“ƒpƒCƒ‹ƒGƒ‰[
+  }
 }
