9300b9dd67a529cad2ccb38f8f93fadeba8336dd
uedayusuke <uedayusuke1107@gmail.com>
2020-10-27 12:48:44 +0900

    [ADD]chap2:変数のスコープ

--- chap2/7/Main.java
+++ chap2/7/Main.java
@@ -1,10 +1,14 @@
 public class Main {
 	public static void main(String[] Args) {
 		int x = 100;
-		x = 200
+		x = 200;
 	}
 
 	public static void method() {
-		// x = 300
+		/*
+		変数には有効範囲(スコープ)があり、ブロック内で宣言されたものを
+		下記のようにブロック外で使用すると"コンパイルエラー"が起こる
+		x = 300;
+		*/
 	}
 }
