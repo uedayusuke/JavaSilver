e45a69f4135347a039e6fed622bf2729f8b1cbc1
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 23:08:58 +0900

    [ADD]chap2:多次元配列

--- chap2/14/Main.java
+++ chap2/14/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
 		int[][] a1 = new int[2][];
-		//int[][] a2 = new int[][2];
-		//int[] a3 = new int[2]{};
-		//int[] a4 = new int[2]{10, 20};
-		int[] a5 = new int[]{10, 20};
-		int[] a6 = {};
+		//int[][] a2 = new int[][2];	1次元目の要素数は省略できない
+		//int[] a3 = new int[2]{};	要素数の指定と、初期化は同時にはできない
+		//int[] a4 = new int[2]{10, 20};	要素数の指定と、初期化は同時にはできない
+		int[] a5 = new int[]{10, 20};	//要素数の指定はなく、初期化のみのため有効
+		int[] a6 = {};	//{}とある為、要素数は自動的に0の配列が生成される・有効
 		int[][] a7 = {};
 		int[][] a8 = new int[][]{};
-		//var a9 = {10, 20};
-		//var a10 = new int[]{10, 20};
+		//var a9 = {10, 20};	varを使用した配列は明示的な型が必要な為エラー
+		var a10 = new int[]{10, 20};	//varを使用し、型を明示しているので有効
  	}
 }
