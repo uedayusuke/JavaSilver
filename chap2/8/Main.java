3fee06cf910e746d460653cfe6d156433578934f
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 15:22:04 +0900

    [ADD]chap2:ローカル変数の型推論

--- chap2/8/Main.java
+++ chap2/8/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-	// var a = 100;
-	// static var b = 100;
+	// var a = 100; メンバ変数(インスタンス変数)にvarは使用不可
+	// static var b = 100; メンバ変数(static変数)にvarは使用不可
 	public static void main(String[] args) {
-		var c = "hello!";
-		var d = 100;
-		// var e;
-		// var f = null;
-		final var g = 100;
+		var c = "hello!"; // "hello!"が代入され、String型
+		var d = 100; // 100が代入され、int型
+		// var e; 初期値を指定せずにvarは使えない
+		// var f = null; nullは型が不明なので不可
+		final var g = 100; // 定数は使用可能
 		// var final h = 100;
 	}
 }
