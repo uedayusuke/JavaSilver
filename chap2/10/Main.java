8531808e193bfbf11856c93d24587e30860a4957
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 16:51:38 +0900

    [ADD]chap2:1次元配列

--- chap2/10/Main.java
+++ chap2/10/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-	public static void main(Stirng[] args) {
+	public static void main(String[] args) {
 		int i;
-		System.out.println("iの値	:" + i);
+		//System.out.println("iの値	:" + i);
 
 		int[] array = new int[1];
 		System.out.println("array[0]の値	:" + array[0]);
-		System.out.println("arrat[0]の値	:" + array[1]);
+		//System.out.println("arrat[0]の値	:" + array[1]);
 	}
 }
