6d0a8187ea8dc00cfa1e99c07e4e331b46dd91e9
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 16:55:47 +0900

    [ADD]chap2:1次元配列

--- chap2/10/Main.java
+++ chap2/10/Main.java
@@ -1,10 +1,18 @@
 public class Main {
 	public static void main(String[] args) {
 		int i;
-		//System.out.println("iの値	:" + i);
+		/*
+		変数iは初期値が設定されていない為、下記を行うと"コンパイルエラー"が起こる
+		System.out.println("iの値	:" + i);
+		*/
 
 		int[] array = new int[1];
+		//配列を生成するとデータ型に合わせて初期値が自動的に作られる為、下記は出力される
 		System.out.println("array[0]の値	:" + array[0]);
-		//System.out.println("arrat[0]の値	:" + array[1]);
+
+		/*
+		下記は、要素数1の配列arrayに対して2番目の要素をしている為、実行時にエラーが起こる
+		System.out.println("arrat[0]の値	:" + array[1]);
+		*/
 	}
 }
