c2e911e9582bba2c07e2651f361b3f065b617609
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 15:17:14 +0900

    [ADD]chap2:ローカル変数の型推論

--- chap2/8/Main.java
+++ chap2/8/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-	var a = 100;
-	static var b = 100;
+	// var a = 100;
+	// static var b = 100;
 	public static void main(String[] args) {
 		var c = "hello!";
 		var d = 100;
-		var e;
-		var f = null;
+		// var e;
+		// var f = null;
 		final var g = 100;
-		var final h = 100;
+		// var final h = 100;
 	}
 }
