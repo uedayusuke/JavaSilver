db2c68756f969a08cdf4c321be1d832474b36b58
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 16:17:56 +0900

    [ADD]chap2:1次元配列

--- chap2/9/Main.java
+++ chap2/9/Main.java
@@ -2,6 +2,24 @@ public class Main {
 	public static void main(String[] args) {
 		char[] c;
 		c = new char[3];
-		System.out.println("cの要素数は" + c.length);
+		System.out.println("cのサイズ:	" + c.length);
+
+		int[] i = new int[4];
+		System.out.println("iのサイズ:	" + i.length);
+
+		String[] str = {"welcome", "to", "Japan"};
+
+		c[0] = 'A';
+		c[1] = 'B';
+		c[2] = 'C';
+
+		i[0] = 100;
+		i[1] = 200;
+		i[2] = 300;
+		i[3] = 400;
+
+		System.out.println("strのサイズ:	" + str.length);
+		System.out.println("c[1]:	" + c[1]);
+		System.out.println("i[2]:	" + i[2]);
 	}
 }
