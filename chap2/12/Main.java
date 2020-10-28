bb5bb12604af7b3e57c6b67f6651475b9954efc0
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 22:14:43 +0900

    [ADD]chap2:多次元配列

---
+++ chap2/12/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(Stirng[] args) {
+		int[][] array = {
+			{10, 20, 30, 40},
+			{50, 60, 70, 80},
+			{100, 200, 300, 400}
+		};
+
+		System.out.println("array[0][0]の値:	" + array[0][0]);
+		System.out.println("array[0][3]の値:	" + array[0][3]);
+		System.out.println("array[2][3]の値:	" + array[2][3]);
+	}
+}
