cf27ce36cf6830ed5a442d1d59b667cb56ae3d44
uedayusuke <uedayusuke1107@gmail.com>
2020-10-28 23:00:08 +0900

    [ADD]chap2:多次元配列

---
+++ chap2/14/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(Stirng[] args) {
+		int[][] a1 = new int[2][];
+		//int[][] a2 = new int[][2];
+		//int[] a3 = new int[2]{};
+		//int[] a4 = new int[2]{10, 20};
+		int[] a5 = new int[]{10, 20};
+		int[] a6 = {};
+		int[][] a7 = {};
+		int[][] a8 = new int[][]{};
+		//var a9 = {10, 20};
+		var a10 = new int[]{10, 20};
+ 	}
+}
