46d3189925ab9aa72a053ec98e568af336734e2d
uedayusuke <uedayusuke1107@gmail.com>
2020-10-29 18:51:30 +0900

    [ADD]chap3:演算子

--- chap3/2/Main.java
+++ chap3/2/Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String[] args) {
-		Strign str = "Hello";
+		String str = "Hello";
 		int a = 10, b = 20;
 		System.out.println(str + a);
 		System.out.println(str + a + b);
