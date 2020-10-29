9bb9211dd9c2bd0a1dd7ea84f01e35496bbd177e
uedayusuke <uedayusuke1107@gmail.com>
2020-10-29 18:54:03 +0900

    [ADD]chap3:コメント

--- chap3/2/Main.java
+++ chap3/2/Main.java
@@ -3,7 +3,7 @@ public class Main{
 		String str = "Hello";
 		int a = 10, b = 20;
 		System.out.println(str + a);
-		System.out.println(str + a + b);
+		System.out.println(str + a + b);	//str+aが実行されたのち+bされるので、Hello30ではなくHello1020となる
 		System.out.println(str + (a + b));
 		System.out.println(a + b + str);
 	}
