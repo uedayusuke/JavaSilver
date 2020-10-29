8495bb9be746e7f636629c799e21eac989fecff3
uedayusuke <uedayusuke1107@gmail.com>
2020-10-29 18:46:06 +0900

    [ADD]chap3:演算子

--- chap3/1/Main.java
+++ chap3/1/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(10 / 3);
-		System.out.println(10 / 3.0);
-		System.out.println(10 % 3);
+		System.out.println(10 / 3); //整数÷整数=整数 (10÷3=3)
+		System.out.println(10 / 3.0); //整数÷小数点数=小数点数 (10÷3.0=3.333.....)
+		System.out.println(10 % 3); //割り算の余り
+		System.out.println(10 % 3.0);
 	}
 }
