bbb36279c843673c27f1fbabfd82aff2a83ad9b1
uedayusuke <uedayusuke1107@gmail.com>
2020-10-29 19:07:37 +0900

    [ADD]chap3:インクリメント・デクリメント

--- chap3/3/Main.java
+++ chap3/3/Main.java
@@ -11,8 +11,10 @@ public class Main {
 		c = 10;
 		d = 10;
 
+		//aにインクリメントした後、bに代入　前置
 		b = ++a;
 		System.out.println("a = " + a + " b = " + b);
+		//cをdに代入した後、cをインクリメント　後置
 		d = c++;
 		System.out.println("c = " + c + " d = " + d);
 	}
