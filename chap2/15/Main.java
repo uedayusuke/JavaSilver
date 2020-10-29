01434ca8027d0783d86e42909e33a24e143b10b4
uedayusuke <uedayusuke1107@gmail.com>
2020-10-29 15:54:36 +0900

    [ADD]chap2:コマンドライン引数

--- chap2/15/Main.java
+++ chap2/15/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-	public class static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println(args[0]);
 		System.out.println(args[1]);
 		System.out.println(args[2]);
