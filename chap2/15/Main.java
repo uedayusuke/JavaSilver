f20a1551af240829139556ceaf9403502094b03d
uedayusuke <uedayusuke1107@gmail.com>
2020-10-29 15:58:07 +0900

    [ADD]chap2:コマンドライン引数

--- chap2/15/Main.java
+++ chap2/15/Main.java
@@ -1,5 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
+		/*
+		コマンドライン引数
+		javaコマンド実行時に引数をプログラムへ渡すことができる
+		→String型の配列として渡される為、演算などには使えない
+		*/
 		System.out.println(args[0]);
 		System.out.println(args[1]);
 		System.out.println(args[2]);
