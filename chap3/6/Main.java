1880190f0709e9297d132c21e6b03b15d579b86c
uedayusuke <uedayusuke1107@gmail.com>
2020-10-30 16:01:10 +0900

    [ADD]chap3:Stringクラス

---
+++ chap3/6/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		String s = "abcdefa";
+		System.out.println("charAt(2)	:" + s.charAt(2));
+		System.out.println("indexOf('c')	:" + s.indexOf('c'));
+		System.out.println("length()	:" + s.length());
+		System.out.println("replace('a'.'z')	;" + s.replace('a'.'z'));
+		System.out.println("substring(2)	:" + s.substring(2));
+	}
+}
