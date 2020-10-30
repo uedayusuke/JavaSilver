707b1bc7aa03a0ac84872b1e3a003a1feadeb23b
uedayusuke <uedayusuke1107@gmail.com>
2020-10-30 15:19:04 +0900

    [ADD]chap3:コメント

--- chap3/4/Main.java
+++ chap3/4/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 	public static void main(String[] args) {
 		int a = 10, b = 20, c = 10;
+
+		/*
+		関係演算子
+		2つの値を比較する際に使用
+		bookean値である"true"、"false"が帰ってくる
+		*/
 		System.out.println("a == b : " + ( a == b));
 		System.out.println("a == c : " + ( a == c));
-		System.out.println("a != b : " + ( a != b));
+		System.out.println("a != b : " + ( a != b)); //"!="は比較する値が異なれば"true"、同じ場合は"false"
 		System.out.println("a != c : " + ( a != c));
 		System.out.println("a < b : " + ( a < b));
 		System.out.println("a <= b : " + ( a <= b));
