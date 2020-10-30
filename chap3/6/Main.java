c5b9b7d510f5f00c5f0945a1802bb955091f1804
uedayusuke <uedayusuke1107@gmail.com>
2020-10-30 16:02:51 +0900

    [ADD]chap3:Stringクラス

--- chap3/6/Main.java
+++ chap3/6/Main.java
@@ -4,7 +4,7 @@ public class Main {
 		System.out.println("charAt(2)	:" + s.charAt(2));
 		System.out.println("indexOf('c')	:" + s.indexOf('c'));
 		System.out.println("length()	:" + s.length());
-		System.out.println("replace('a'.'z')	;" + s.replace('a'.'z'));
+		System.out.println("replace('a','z')	;" + s.replace('a','z'));
 		System.out.println("substring(2)	:" + s.substring(2));
 	}
 }
