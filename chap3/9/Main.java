d375834a08f6c68b61ce208fee280e094f9a7e39
uedayusuke <uedayusuke1107@gmail.com>
2020-11-01 16:29:33 +0900

    [ADD]chap3:関係演算子

--- chap3/9/Main.java
+++ chap3/9/Main.java
@@ -2,10 +2,12 @@ public class Main {
 	public static void main(String[] args) {
 		int[] a1 = {10};
 		int[] a2 = {10};
+		//見た目は同じであるが、別々に作られ、参照先も異なるため"false"
 		System.out.println("a1 == a2	:" + (a1 == a2));
 
 		int[] a3 = {10};
 		int[] a4 = a3;
+		//同一のデータを参照しているため"true"
 		System.out.println("a3 == a4	:" + (a3 == a4));
  	}
 }
