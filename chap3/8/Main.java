bc04379ef7295761dec1217c0dd4ff3ca5bbc4d8
uedayusuke <uedayusuke1107@gmail.com>
2020-11-01 16:15:01 +0900

    [ADD]chap3:関係演算子

---
+++ chap3/8/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 1_0;
+		long n3 = 10L;
+		byte n4 = 10;
+		char n5 = 'a';
+		float n6 = 10.0F;
+		double n7 = 10.0;
+		boolean n8 = true;
+
+		System.out.println(n2);
+	}
+}
