ac8f03800ade87c9777b05afce3db5861e7b554f
uedayusuke <uedayusuke1107@gmail.com>
2020-11-07 14:43:55 +0900

    [ADD]chap3:文字列の比較

--- chap3/10/Main.java
+++ chap3/10/Main.java
@@ -3,5 +3,9 @@ public class Main {
 		String s1 = "tanaka";
 		String s2 = "tanaka";
 		System.out.println("s1.equals(s2)	:" + (s1.equals(s2)));
+		System.out.println("s1 == s2	:" + (s1 == s2));
+
+		String s3 = "TANAKA";
+		System.out.println("s1.equals(s3)	:" + (s1.equals(s3)));
 	}
 }
