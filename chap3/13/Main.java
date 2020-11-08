c2e84523e1fc83db663f9f2775db07792cf18925
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 15:00:09 +0900

    [ADD]chap13:null比較

--- chap3/13/Main.java
+++ chap3/13/Main.java
@@ -9,9 +9,14 @@ public class Main {
 		String str2 = "";
 		int[] array1 = null;
 		int[] array2 = new int[1];
+
 		System.out.println(str1 == null);
+
+		//""（空文字）はnullとは違い、メモリ領域は確保されているため"false"
 		System.out.println(str2 == null);
 		System.out.println(array1 != null);
+
+		//領域の確保はされているため"true"
 		System.out.println(array2 != null);
 	}
 }
