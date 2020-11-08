7d31d7fc2639c81dc32ab75f8f0f9546d5652e31
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 15:19:26 +0900

    [ADD]chap3:キャスト演算子

---
+++ chap3/14/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		double d = 10.5;
+		//int i = d;
+		int i = (int)d;
+
+		System.out.println("iの値:" + i);
+
+		//foo(i);
+		foo((short)i);
+	}
+
+	static void foo(short a) {
+		System.out.println("aの値:" + a);
+	}
+}
