176aa5f8f7f39b34ba63403ca9153a39d5241ebd
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 17:43:26 +0900

    [ADD]chap3:if文

---
+++ chap3/16/Sub2.java
@@ -0,0 +1,11 @@
+public class Sub2 {
+	public static void main(String[] aegs) {
+		boolean b1 = false;
+		boolean b2 = true;
+
+		if (b1 = b2) {
+			System.out.println("b1	:" + b1);
+			System.out.println("b2	:" + b2);
+		}
+	}
+}
