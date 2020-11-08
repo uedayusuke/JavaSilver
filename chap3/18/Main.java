24c43904379f66f6d8de7d556f7ae473c1b3bdc0
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 18:14:29 +0900

    [ADD]chap3:elseif文

--- chap3/18/Main.java
+++ chap3/18/Main.java
@@ -2,6 +2,15 @@ public class Main {
 	public static void main(String[] args) {
 		char c = 'p';
 
+		/*
+		if(条件式1){
+			処理文; //条件式1が"true"の時に実行
+		}else if(条件式2){
+			処理文; //条件式1が"false"で条件式2が"true"の時に実行
+		}else{
+			条件式1.2共に"false"の時に実行
+		}
+		*/
 		if(c == 'a'){
 			System.out.println("c の値は a です。");
 		}else if(c == 'b'){
