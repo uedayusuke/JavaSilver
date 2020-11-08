2f910969aac238959af9ba75a6b7a4b7efc2fc86
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 14:55:22 +0900

    [ADD]chap13:null比較

---
+++ chap3/13/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		String str1 = null;
+		String str2 = "";
+		int[] array1 = null;
+		int[] array2 = new int[1];
+		System.out.println(str1 == null);
+		System.out.println(str2 == null);
+		System.out.println(array1 != null);
+		System.out.println(array2 != null);
+	}
+}
