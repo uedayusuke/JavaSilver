a190d24944fe790f5d45984581bddbafa3267b1c
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 17:08:50 +0900

    [ADD]chap3:BoxingとUnboxing

---
+++ chap3/15/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		int i1 = 100;
+		Integer obj = i1;
+		int i2 = obj;
+		method(i2);
+	}
+
+	static void method(Integer obj) {
+		int i = obj + 100;
+		System.out.println(i);
+	}
+}
