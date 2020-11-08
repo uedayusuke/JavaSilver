dbe727e100604ced22ce022d24f9ee41e68777ae
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 17:47:00 +0900

    [ADD]chap3:if文

--- chap3/16/Sub2.java
+++ chap3/16/Sub2.java
@@ -1,11 +1,16 @@
 public class Sub2 {
 	public static void main(String[] aegs) {
-		boolean b1 = false;
-		boolean b2 = true;
+		boolean b1 = true;
+		boolean b2 = false;
 
-		if (b1 = b2) {
+		if (b2 = b1) {
 			System.out.println("b1	:" + b1);
 			System.out.println("b2	:" + b2);
 		}
+
+		/*
+		条件の箇所で代入演算子を用いているが
+		結果がboolean値で返るため、if文として問題なし！
+		*/
 	}
 }
