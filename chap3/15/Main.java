641a9858e07d133527310413a5437a68d1ef526e
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 17:16:41 +0900

    [ADD]chap3:BoxingとUnboxing

--- chap3/15/Main.java
+++ chap3/15/Main.java
@@ -1,11 +1,19 @@
 public class Main {
 	public static void main(String[] args) {
+		/*
+		ラッパークラスをしようする事で、基本データ型の値を参照型として扱うことができる
+		変換は自動的に行われる
+		基本データ型 → 参照型 :Boxing
+		参照型 → 基本データ型 :Unboxing
+		*/
+
 		int i1 = 100;
-		Integer obj = i1;
-		int i2 = obj;
-		method(i2);
+		Integer obj = i1; //Boxing
+		int i2 = obj; //Unboxing
+		method(i2); //int型で引数を渡す
 	}
 
+	//Integer型で受け取る
 	static void method(Integer obj) {
 		int i = obj + 100;
 		System.out.println(i);
