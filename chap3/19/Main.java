09fae8bbbcfbfea3fa5c19b36dc3b1c38e987f3b
uedayusuke <uedayusuke1107@gmail.com>
2020-11-08 23:44:27 +0900

    [ADD]chap3:条件演算子

--- chap3/19/Main.java
+++ chap3/19/Main.java
@@ -3,6 +3,15 @@ public class Main {
 		int num = 20;
 		String str = "numの値は";
 
+		/*
+		条件演算子
+		条件式 ? 式1 : 式2
+
+		条件式が"true"の時 → 式1を実行
+		条件式が"false"の時 → 式2を実行
+		*/
+
+		//"+="を用いて文字列を結合
 		str += num < 10 ? "10未満" : "10以上";
 		System.out.println(str);
 	}
