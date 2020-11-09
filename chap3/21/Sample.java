72694feaebf96e62c762dc04036d79e6c0ecb10a
uedayusuke <uedayusuke1107@gmail.com>
2020-11-09 22:24:07 +0900

    [ADD]switch文

--- chap3/21/Sample.java
+++ chap3/21/Sample.java
@@ -2,6 +2,7 @@ public class Sample {
 	public static void main(String[] args) {
 		int num = 2;
 
+		//"num++"は式の評価後インクリメントされるため、この場合"case 2"が返る
 		switch(num++){
 			case 1:
 				System.out.println("nの値は1です。");
@@ -13,6 +14,7 @@ public class Sample {
 				System.out.println("nの値は1でも2でもないです。");
 		}
 
+		//switch文評価後にインクリメントされているため、numは"3"
 		System.out.println("switch文処理後のnの値は" + num+ "です。");
 	}
 }
