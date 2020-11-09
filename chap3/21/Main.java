17b6283ae10558777568f7cfe40d47067618aa8d
uedayusuke <uedayusuke1107@gmail.com>
2020-11-09 22:02:59 +0900

    [ADD]chap3:switch文の使用例

--- chap3/21/Main.java
+++ chap3/21/Main.java
@@ -2,6 +2,17 @@ public class Main {
 	public static void main(String[] args) {
 		int num = 2;
 
+		/*
+		switch(式){
+			case 定数1:	//式の結果が定数1と一致した時、以下の処理文を実行。
+				処理文;
+			case 定数2:	//式の結果が定数2と一致した時、以下の処理文を実行。
+				処理文;
+				break;	//処理を実行したのち、switch文を抜ける。
+			default:	//式の結果が、上記のどのcaseにも一致しない時、以下の処理文を実行。
+				defaultの処理文;
+		}
+		*/
 		switch(num){
 			case 1:
 				System.out.println("numの値は1です。");
