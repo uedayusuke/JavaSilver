8630b1da1ea95ac19515941aacb9823d7b1a32aa
uedayusuke <uedayusuke1107@gmail.com>
2020-11-09 22:34:37 +0900

    [ADD]chap3:switch文

--- chap3/22/Main.java
+++ chap3/22/Main.java
@@ -3,13 +3,13 @@ public class Main {
 		int num = 14;
 
 		switch(num){
-			case 1:
+			case 1:	//break文がないため"case 2"の処理文が実行される
 			case 2:
 				//numの値が1か2の時実行
 				System.out.println("numの値は1または2");
 				break;
 			case 3:
-			case 4:
+			case 4:	//break文がないため"case 4"の処理文が実行される
 				//numの値が3か4の時実行
 				System.out.println("numの値は3または4");
 				break;
