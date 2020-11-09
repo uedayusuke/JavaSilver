91cf9f08e06fe994c2f37833147ca748cb509ac5
uedayusuke <uedayusuke1107@gmail.com>
2020-11-09 22:14:56 +0900

    [ADD]chap3:switch文の使用例

---
+++ chap3/21/Sub.java
@@ -0,0 +1,16 @@
+public class Sub {
+	public static void main(String[] args) {
+		int n = 21;
+
+		switch(n){
+			case 10:
+				System.out.println("nの値は10");
+				break;
+			case 20:
+				System.out.println("nの値は20");
+				break;
+			default:
+				System.out.println("nは10でも20でもありません");
+		}
+	}
+}
