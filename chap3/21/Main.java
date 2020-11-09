28754482b5cfa155b04ceae8c7b51d09d0c5071a
uedayusuke <uedayusuke1107@gmail.com>
2020-11-09 21:56:46 +0900

    [ADD]chap3:switch文の使用例

---
+++ chap3/21/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		int num = 2;
+
+		switch(num){
+			case 1:
+				System.out.println("numの値は1です。");
+				break;
+			case 2:
+				System.out.println("numの値は2です。");
+			default:
+				System.out.println("defaultです。");
+		}
+	}
+}
