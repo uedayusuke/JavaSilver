b9a76c5ceb4041d324ddcf5362237d3840cf3035
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 21:35:12 +0900

    [ADD]chap4:while文とdo-while文の比較

--- chap4/4/Main.java
+++ chap4/4/Main.java
@@ -2,12 +2,22 @@ public class Main{
 	public static void main(String[] args) {
 		int count = 5;
 
+		/*
+		whileの場合
+		条件式の"count != 5"でfalseが返るため一度も実行されない
+		*/
 		while(count != 5 && count > 0){
 			System.out.println("while	: count = " + count);
 			count--;
 		}
 
+		//変わっていないが明示的に再代入
 		count = 5;
+
+		/*
+		do-while文
+		最初に処理文が実行されるため、ディクリメントされ、5~1が出力される
+		*/
 		do{
 			System.out.println("do-while	: count = " + count);
 			count--;
