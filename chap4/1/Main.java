53ebb7788cb8da0b7b9e9abca3bbc70ccc57963c
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 21:07:30 +0900

    [ADD]chap4:while文

---
+++ chap4/1/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		int num = 0;
+
+		/*
+		while(条件式){
+			処理文; 条件式がtrueの間処理文が実行される
+		}
+		*/
+		while(num < 5){
+			System.out.println(num + " ");
+			num++;
+		}
+	}
+}
