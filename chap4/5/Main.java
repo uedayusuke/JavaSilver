722166f3bac1bf5d33b63fc9436fef07a1b912ff
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 21:44:57 +0900

    [ADD]chap4:for文

--- chap4/5/Main.java
+++ chap4/5/Main.java
@@ -1,5 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
+		/*
+		for文
+		for(式1; 式2; 式3){
+			処理文;
+		}
+		式1:変数の宣言(カウンタ変数)
+		式2:条件式
+		式3:カウンタ変数を更新する式
+		*/
 		for(int count = 0; count < 5; count++){
 			System.out.println(count + " ");
 		}
