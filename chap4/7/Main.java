f9244cae1c1e6e270b4e58d8efdc78b10fa3f6bb
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 22:13:00 +0900

    [ADD]chap4:拡張for文

--- chap4/7/Main.java
+++ chap4/7/Main.java
@@ -2,10 +2,21 @@ public class Main{
 	public static void main(String[] args) {
 		char[] array = {'a', 'b', 'c', 'd', 'e'};
 
+		/*
+		拡張for文
+		for(変数宣言 : 参照変数名){
+			処理文;
+		}
+
+		変数宣言で宣言する型は、参照変数の型と合わせる
+		*/
+
+		//拡張for文
 		for(char c : array){
 			System.out.println(c + " ");
 		}
 
+		//for文
 		System.out.println();
 
 		for(int count = 0; count < array.length; count++){
