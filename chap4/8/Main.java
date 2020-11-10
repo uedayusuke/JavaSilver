5e539f965a8dfae26520b14aa6b217b1b8837cca
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 22:26:38 +0900

    [ADD]chap4:制御文

--- chap4/8/Main.java
+++ chap4/8/Main.java
@@ -1,9 +1,16 @@
 public class Main{
 	public static void main(String[] args) {
+		//親の制御文"for"
 		for(int i = 1; i < 10; i++){
+			//入れ子の制御文"if"
 			if((i % 4) == 0){
 				System.out.println(i + "は 4 の倍数です。");
 			}
 		}
+
+		/*
+		forでは1~9が繰り返し処理され、
+		そのなかでifの条件に合うものが出力される
+		*/
 	}
 }
