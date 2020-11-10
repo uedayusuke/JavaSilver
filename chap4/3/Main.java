557c5ae95f4023595f3d5c7eb506c1c63ebfb5ce
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 21:24:08 +0900

    [ADD]chap4:do-while文

--- chap4/3/Main.java
+++ chap4/3/Main.java
@@ -2,6 +2,16 @@ public class Main {
 	public static void main(String[] args) {
 		int num = 0;
 
+		/*
+		do-while文
+		do{
+			処理文;
+		}while(条件式);
+
+		while文とは異なりまず繰り返し処理が行われる
+		→一回は必ず繰り返される
+		*/
+
 		do{
 			System.out.println(num + " ");
 			num++;
