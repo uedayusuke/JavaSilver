c32653e49705bf8db7b1920a64d373bd4b1b6a7e
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 21:15:04 +0900

    [ADD]chap4:while文

---
+++ chap4/2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int num = 0;
+
+		//条件式で"false"が返るため、何も実行されない
+		while(num > 0){
+			System.out.println(num + " ");
+			num--; //numをディクリメント
+		}
+	}
+}
