8c1fdf59977a342b07a267400536ceb38699777e
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 22:48:44 +0900

    [ADD]chap4:continue文

---
+++ chap4/10/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+	public static void main(String[] args) {
+		for(int i = 1; i < 10; i++){
+			if((i % 3) == 0){
+				System.out.println("処理をスキップします");
+				continue;
+			}
+			System.out.println("i = " + i);
+		}
+		System.out.println("for文後の処理");
+	}
+}
