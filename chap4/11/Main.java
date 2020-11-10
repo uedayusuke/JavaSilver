21fb9740aecb59477bc22513d7f47f8c18e5a7f1
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 23:14:44 +0900

    [ADD]chap4:ラベル

--- chap4/11/Main.java
+++ chap4/11/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
+		//ラベル"loop1"を設定
 		loop1:
 		for(int x = 0; x < 3; x++){
 
@@ -7,6 +8,10 @@ public class Main {
 				System.out.println("x = " + x + " y = " + y);
 				if(x == 1 && y == 1){
 					System.out.println("break文の実行");
+					/*
+					ラベル"loop1"にbreak
+					→外側のfor文処理が終了
+					*/
 					break loop1;
 				}
 			}
@@ -14,6 +19,7 @@ public class Main {
 
 		System.out.println("------------------------");
 
+		//ラベル"loop2"を設定
 		loop2:
 		for(int x = 0; x < 3; x++){
 
@@ -21,6 +27,10 @@ public class Main {
 				System.out.println("x = " + x + " y = " + y);
 				if(x == 1 && y == 1){
 					System.out.println("continue文の実行");
+					/*
+					ラベル"loop2"にcontinue
+					→内側のfor文の処理をスキップし、外側のfor文に制御を移す
+					*/
 					continue loop2;
 				}
 			}
