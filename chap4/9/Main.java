a1d738266187ef7e5773c2cb93e6df8f4fcca489
uedayusuke <uedayusuke1107@gmail.com>
2020-11-10 22:40:43 +0900

    [ADD]chap4:break文

--- chap4/9/Main.java
+++ chap4/9/Main.java
@@ -1,6 +1,11 @@
 public class Main{
 	public static void main(String[] args) {
+		//下記のfor文は式2が省略されているため無限ループが起きる
 		for(int i = 0; ; i++){
+			/*
+			if文に"break"を設置しiが3になった時にbreak文で抜ける
+			if文からではなく、for文事体から抜ける!!
+			*/
 			if(i == 3){
 				break;
 			}
