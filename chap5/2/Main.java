b4e67ddbc3087b36014cda8ca868e0f04593e476
uedayusuke <uedayusuke1107@gmail.com>
2020-11-12 15:59:44 +0900

    [ADD]chap5:クラスの定義とインスタンス化

--- chap5/2/Main.java
+++ chap5/2/Main.java
@@ -1,9 +1,24 @@
 class Employee {
 	int id;
 
+	/*
+	メソッド:クラスが持つべき操作・振る舞い
+
+	[修飾子] 戻り値の型 メソッド名 (引数リスト){}
+	→呼び出し元に戻り値を返す必要がない場合は"void"
+	 呼び出し元から引数を受け取らない場合は"引数リスト"は空白でも良い
+	*/
+
 	void setId(int i){
 		id = i;
 	}
+
+	/*
+	return文
+	呼び出し元に戻り値を返す場合"return"を記述
+
+	return 戻す値;
+	*/
 
 	int getId(){
 		return id;
@@ -12,8 +27,17 @@ class Employee {
 
 public class Main {
 	public static void main(String[] args) {
+
+		/*
+		インスタンス化:クラスを元にオブジェクトを生成すること
+		クラス名 インスタンス名(変数名) = new クラス名();
+		*/
 		Employee a = new Employee();
 
+		/*
+		インスタンス名.メソッド名
+		aは"Id = 100"を保持
+		*/
 		a.setId(100);
 
 		Employee b = new Employee();
