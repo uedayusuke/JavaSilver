92d02eff10e5680165171a1dc4541f6eb2b345e7
uedayusuke <uedayusuke1107@gmail.com>
2020-11-14 18:31:43 +0900

    [ADD]chap5:staticイニシャライザとコンストラクタ

--- chap5/12/Main.java
+++ chap5/12/Main.java
@@ -1,19 +1,31 @@
+/*
+staticイニシャライザ
+クラスファイルがロードされたタイミングで実行されるプログラム
+→インスタンス化する前やmain()メソッドを呼び出す前に実行したい処理をかく
+*/
+
 class Test {
 	static {
+		//3
 		System.out.println("Testクラス　：　stasicイニシャライズ");
 	}
 
 	Test() {
+		//4
 		System.out.println("Testクラス　：　コンストラクト");
 	}
 }
 
 public class Main {
 	static {
+		//1
 		System.out.println("Mainクラス　：　staticイニシャライズ");
 	}
 	public static void main(String[] args) {
+		//2
 		System.out.println("Mainクラス　：　mainメソッド");
+
+		//Testクラスをインスタンス化することでTestクラスのstasicイニシャライズが実行される
 		Test obj = new Test();
 	}
 }
