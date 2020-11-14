1d28427e46910f882fcb8d5316da38dd36066983
uedayusuke <uedayusuke1107@gmail.com>
2020-11-14 18:10:19 +0900

    [ADD]chap5:stasicメンバのクラス内アクセス

--- chap5/10/Test.java
+++ chap5/10/Test.java
@@ -1,25 +1,32 @@
 public class Test {
+	//インスタンス変数
 	int instanceVal;
+	//メンバ変数
 	static int staticVal;
 
+	//インスタンス　→ インスタンス　ok
 	int methodA() {
 		return instanceVal;
 	}
 
+	//インスタンス　→ stasic ok
 	int methodB() {
 		return staticVal;
 	}
 
 	/*
+	stasic　→　インスタンス　NG
 	static int methodC() {
 		return instanceVal;
 	}
 	*/
 
+	//static　→ インスタンス　ok
 	static int methodD() {
 		return staticVal;
 	}
 
+	//インスタンス化すれば　stasic　→ インスタンス　ok
 	static int methodE() {
 		Test obj = new Test();
 		return obj.instanceVal;
