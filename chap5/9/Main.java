1c8f4e21f50c31469dd131ee87a2e43fb419b135
uedayusuke <uedayusuke1107@gmail.com>
2020-11-14 17:53:11 +0900

    [ADD]chap5:staticメソッドの呼び出し

--- chap5/9/Main.java
+++ chap5/9/Main.java
@@ -19,3 +19,18 @@ class Test {
 		System.out.println("methodB():" + staticVal);
 	}
 }
+
+public class Main {
+	public static void main(String[] args) {
+		//System.out.println(Test.instanceVal);
+		System.out.println(Test.staticVal);
+		//Test.methodA();
+		Test.methodB();
+
+		Test obj = new Test();
+		System.out.println(obj.instanceVal);
+		System.out.println(obj.staticVal);
+		obj.methodA();
+		obj.methodB();
+	}
+}
