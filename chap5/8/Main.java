56e707ca43e5028fa9ad9141b4cdf324c61a1895
uedayusuke <uedayusuke1107@gmail.com>
2020-11-14 15:40:24 +0900

    [ADD]chap5:可変長引数のオーバーロード

--- chap5/8/Main.java
+++ chap5/8/Main.java
@@ -1,19 +1,34 @@
 class Test {
+	/*
+	可変長引数のオーバーロード
+	完全一致 > 暗黙の型変換 > Boxing > 可変長引数
+	*/
+
 	public void method(int a){
 		System.out.println("method(int a)");
 	}
 
+	//暗黙の型変換
 	public void method(long a){
 		System.out.println("method(long a)");
 	}
 
+	//Boxing
 	public void method(Integer a){
 		System.out.println("method(Integer a)");
 	}
 
+	//可変長引数
 	public void method(int... a){
 		System.out.println("method(int... a)");
 	}
+
+	/*
+	可変長引数は配列に変換されるためコンパイルエラー
+	public void method(int[] a){
+		System.out.println("method(int[] a)")
+	}
+	*/
 }
 
 public class Main {
