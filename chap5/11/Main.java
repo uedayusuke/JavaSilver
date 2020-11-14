2880c8ab6389551557760cd6067623956a803317
uedayusuke <uedayusuke1107@gmail.com>
2020-11-14 18:21:32 +0900

    [ADD]chap5:nullに対するメンバの呼び出し

--- chap5/11/Main.java
+++ chap5/11/Main.java
@@ -5,8 +5,10 @@ class Test {
 
 public class Main {
 	public static void main(String[] args) {
+		//インスタンス化ではない
 		Test obj = null;
 
+		//staticはインスタンス化を必要とせず、クラスが読み込まれれば実行可能
 		System.out.println(obj.staticValue);
 		//System.out.println(obj.instanceValue);
 	}
