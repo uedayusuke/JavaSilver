41ff5268304f912f06080c7f8ad4b0b2df7b96ec
uedayusuke <uedayusuke1107@gmail.com>
2020-11-14 14:29:59 +0900

    [ADD]chap5:可変長引数

---
+++ chap5/6/Main.java
@@ -0,0 +1,19 @@
+class Test {
+	public void method(String s, int... a) {
+		System.out.println(s + "サイズ	: " + a.length);
+		for(int i : a) {
+			System.out.println("　第２引数の値	:" + i);
+		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Test obj = new Test();
+		int[] array = {10, 20, 30};
+		obj.method("1回目");
+		obj.method("2回目", 10);
+		obj.method("3回目", 10, 20);
+		obj.method("4回目", array);
+	}
+}
