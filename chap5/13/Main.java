9d12925b2e8ab8a3ceea382f47b9a173374f4f35
uedayusuke <uedayusuke1107@gmail.com>
2020-11-15 12:00:34 +0900

    [ADD]chap5:public修飾子とprivate修飾子

--- chap5/13/Main.java
+++ chap5/13/Main.java
@@ -1,6 +1,15 @@
 class Employee {
+	/*
+	private:
+	同一クラス内からのみ利用可能
+	*/
 	private int id;
 
+	/*
+	public:
+	どのクラスからでもアクセス可能
+	*/
+	//コンストラクタ
 	public Employee(int i){
 		id = i;
 	}
@@ -12,12 +21,15 @@ class Employee {
 
 public class Main {
 	public static void main(String[] args) {
+		//インスタンス化
 		Employee emp = new Employee(100);
 
 		/*
+		"private"を他クラスからアクセスしているためコンパイルエラー
 		System.out.println("private指定のインスタンス変数のアクセス : " + emp.id);
 		*/
 
+		//"public"はどのクラスからもアクセス可
 		System.out.println("public指定のメソッドへアクセス　：　" + emp.getId());
 	}
 }
