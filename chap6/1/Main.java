db7aa630ccb3d84420cb228421311aa232eed016
uedayusuke <uedayusuke1107@gmail.com>
2020-11-15 13:13:02 +0900

    [ADD]chap6:クラスの継承

--- chap6/1/Main.java
+++ chap6/1/Main.java
@@ -1,3 +1,23 @@
+/*
+継承:
+既存のクラスをもとに新たなクラスを定義すること
+サブクラスをインスタンス化するとスーパークラスで定義したメソッドを引き継ぐことができる
+また、"private"修飾子のメンバは引き継げない
+
+[修飾子] class サブクラス名 extends スーパークラス名 {}
+*/
+
+/*
+スーパークラス:
+継承の元となるクラス
+*/
+
+/*
+サブクラス:
+スーパークラスを元に新たに定義されるクラス
+*/
+
+//スーパークラス
 class Employee {
 	private String id = "100";
 	public String getId() {
@@ -5,6 +25,7 @@ class Employee {
 	}
 }
 
+//サブクラス
 class Sales extends Employee {
 	private String clientName = "Ueda";
 	public String getClientName() {
@@ -14,10 +35,13 @@ class Sales extends Employee {
 
 public class Main {
 	public static void main(String[] args) {
+		//サブクラスをインスタンス化
 		Sales s = new Sales();
 
+		//サブクラスのメソッドを呼び出し
 		System.out.println("clientName	：" + s.getClientName());
 
+		//スーパークラスのメソッドを呼び出し
 		System.out.println("id	：" + s.getId());
 	}
 }
