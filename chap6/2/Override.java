f03c0eaaa3d63d23635addbaddbe64558facb217
uedayusuke <uedayusuke1107@gmail.com>
2020-11-16 21:35:40 +0900

    [ADD]chap6:@Override

--- chap6/2/Override.java
+++ chap6/2/Override.java
@@ -4,5 +4,18 @@ class X {
 }
 
 class Y extends X {
+	/*
+	本当はclassXのmethodAをオーバーライドする予定がタイプミスでmethodaに
+	しかし、classYの独自のメソッドと判断される為エラーは起こらない
+	*/
+	void methoda() {}
+}
+
+class Z extends X {
+	/*
+	@Overrideと明記することでオーバーライドすることを明示でき
+	スペルミス等がありオーバーライドできない場合はコンパイルエラーを起こす
+	*/
+	@Override
 	void methoda() {}
 }
