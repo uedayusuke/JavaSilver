10512cbc25fabd7f9336dd52ab92ce5c59f9cbe7
uedayusuke <uedayusuke1107@gmail.com>
2020-11-17 11:20:33 +0900

    [ADD]chap6:final修飾子を使用したクラス・メソッド

---
+++ chap6/4/Test.java
@@ -0,0 +1,24 @@
+class SuperA {}
+final class SuperB {}
+
+class SuperC {
+	void print() {}
+}
+
+class SuperD {
+	final void print() {}
+}
+
+class SubA extends SuperA {}
+
+//class SubB extends SuperB {}
+
+class SubC extends SuperC {
+	void print() {}
+}
+
+/*
+class SubD extends SuperD {
+	void print() {}
+}
+*/
