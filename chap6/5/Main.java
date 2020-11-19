3160e368b6eab1da71e023d66fb26cd68b69ffe6
uedayusuke <uedayusuke1107@gmail.com>
2020-11-19 11:34:44 +0900

    [ADD]chap6:コンストラクタにthis

--- chap6/5/Main.java
+++ chap6/5/Main.java
@@ -1,4 +1,15 @@
 class Foo {
+	/*
+	this：自分自身（自オブジェクト）を表現
+	     自オブジェクトが保持する変数・メソッド・コンストラクタを明示的に指定
+	     this.変数名で自オブジェクトが持つ変数、つまりインスタンス変数を示す
+	*/
+
+	/*
+	thisでコンストラクタ
+	"this()"と記述する事で、コンストラクタの中から、自クラスで定義した、別のコンストラクタを呼び出すことができる
+	→ this()はコンストラクタ定義の"先頭"に記述!　
+	*/
 	String str;
 	int num;
 
@@ -20,6 +31,7 @@ class Foo {
 
 public class Main {
 	public static void main(String[] args) {
+		//それぞれのコンストラクタを呼び出す
 		Foo f1 = new Foo();
 		Foo f2 = new Foo("Hallo");
 		Foo f3 = new Foo("Bye", 200);
