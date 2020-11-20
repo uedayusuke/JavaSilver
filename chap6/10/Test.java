00cde247213bb09a6f10282bd22aeb434672e958
uedayusuke <uedayusuke1107@gmail.com>
2020-11-20 11:59:32 +0900

    [ADD]chap6:抽象クラスを継承した具象クラス

--- chap6/10/Test.java
+++ chap6/10/Test.java
@@ -1,9 +1,14 @@
+//抽象クラス
 abstract class X {
+	//抽象メソッド
 	protected abstract void methodA();
 }
 
+//抽象クラス
 abstract class Y {}
 
+//具象クラス
 class Z extends Y {
+	//具象メソッド　オーバーライド
 	protected void methodA(){}
 }
