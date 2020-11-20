ff616e0b5e58f2fa337219c51e1d844a733a45fd
uedayusuke <uedayusuke1107@gmail.com>
2020-11-20 11:40:04 +0900

    [ADD]chap6:抽象クラスを継承した具象クラス

--- chap6/9/Test.java
+++ chap6/9/Test.java
@@ -1,11 +1,18 @@
+//抽象クラス
 abstract class SuperA {
+	//抽象メソッド
 	protected abstract void methodA();
+
+	//具象メソッド
 	public void methodB(){}
 }
 
+//具象クラス
 class SubA extends SuperA {
+	//抽象メソッドをオーバーライド 必須!!
 	protected void methodA(){}
 	//public void methoda(){}
 
+	//具象メソッドのオーバーライドは任意
 	public void methodB(){}
 }
