eb172b1f14b66e6dadac80fc250535361d5997af
uedayusuke <uedayusuke1107@gmail.com>
2020-11-20 13:49:06 +0900

    [ADD]chap6:インタフェースの実装とクラスの継承

--- chap6/13/Test.java
+++ chap6/13/Test.java
@@ -2,10 +2,14 @@ interface MyInter {
 	void methodA();
 }
 
-class Super {
-	void methodB() {}
+abstract class Super {
+	abstract void methodB();
 }
 
+/*
+実装と継承を同時に行う場合は、継承を先に記述する
+*/
 class MyClass extends Super implements MyInter {
+	void methodB() {}
 	public void methodA() {}
 }
