f61963365a8d6ca41f71e69b1ea6fff3634bd244
uedayusuke <uedayusuke1107@gmail.com>
2020-11-20 13:41:56 +0900

    [ADD]chap6:インタフェースと実装クラス

--- chap6/12/Main.java
+++ chap6/12/Main.java
@@ -1,18 +1,39 @@
+/*
+インタフェース：
+公開すべき必要な操作をまとめたクラス、取り決め。
+→ [アクセス修飾子] interface インタフェース名 {}
+*/
+
+/*
+インタフェースの実装クラス；
+インタフェースはインスタンス化できないため、実装クラスで抽象メソッドをオーバーライドする
+オーバーライドするときは必ず"public修飾子"をつける
+また複数のインタフェースをカンマ区切りで同時に指定できる
+→ [修飾子] class クラス名 implements インタフェース名 {}
+*/
+
+//インタフェース
 interface MyInter1 {
+	//抽象メソッド
 	double methodA(int num);
+	//デフォルト（具象）メソッド
 	default void methodB() {
 		System.out.println("methodB()");
 	}
 }
 
 interface MyInter2 {
+	//抽象メソッド
 	int methodC(int val1, int val2);
+	//staticメソッド
 	static void methodD() {
 		System.out.println("methodD");
 	}
 }
 
+//実装クラス
 class MyClass implements MyInter1, MyInter2 {
+	//オーバーライド
 	public double methodA(int num) {
 		return num * 0.3;
 	}
@@ -24,14 +45,17 @@ class MyClass implements MyInter1, MyInter2 {
 
 public class Main {
 	public static void main(String[] args) {
+		//インスタンス化
 		MyClass obj = new MyClass();
 
 		System.out.println("methodA	:" + obj.methodA(10));
 
 		System.out.println("methodC	:" + obj.methodC(10, 20));
 
+		//デフォルトメソッドの呼び出し
 		obj.methodB();
 
+		//stasicメソッドはクラス名.メソッド名で呼び出せる
 		MyInter2.methodD();
 	}
 }
