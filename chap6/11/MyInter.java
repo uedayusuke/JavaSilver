59a553353103cc499ddd08f251670db09d2259af
uedayusuke <uedayusuke1107@gmail.com>
2020-11-20 13:01:10 +0900

    [ADD]chap6:デフォルトメソッドの処理

---
+++ chap6/11/MyInter.java
@@ -0,0 +1,13 @@
+interface MyInter {
+	public default boolean equals(Object obj) {
+		return true;
+	}
+
+	public default int hashCode() {
+		return 10;
+	}
+
+	public default String toString() {
+		return "hello";
+	}
+}
