30ce94a6b6a4eea0b74179005cddbeffbe658835
uedayusuke <uedayusuke1107@gmail.com>
2020-11-22 14:36:48 +0900

    [ADD]chap6:継承時のprivate

--- chap6/15.1/Private.java
+++ chap6/15.1/Private.java
@@ -13,7 +13,9 @@ class B extends A {
 
 public class Private {
 	public static void main(String[] args) {
+		//サブクラスをインスタンス化して、スーパークラスの型に代入
 		A obj = new B();
+		//コンパイル時、objの型を確認、A型、Aクラスのfoo()メソッドをチェックという検証を行い、privateのためコンパイルエラー
 		obj.foo();
 	}
 }
