2ba25ba8d4e16c028c02e0ab540370e6aca34f99
uedayusuke <uedayusuke1107@gmail.com>
2020-11-22 14:48:04 +0900

    [ADD]chap6:instanceof演算子

--- chap6/16/Main.java
+++ chap6/16/Main.java
@@ -6,6 +6,15 @@ class D {}
 public class Main {
 	public static void main(String[] args) {
 		C obj = new C();
+
+		/*
+		instanceof演算子
+		特定のオブジェクトが特定の型を持つかどうか判定
+		→ 参照変数名 instanceof クラス名またはインタフェース名
+		左辺で指定した変数に右辺で指定した型を持つ　true
+		右辺がスーパークラスやインタフェースで左辺がそのサブクラスや実装クラス　true
+		両辺に継承関係がない場合はコンパイルエラー
+		*/
 		System.out.println(obj instanceof A);
 		System.out.println(obj instanceof B);
 		System.out.println(obj instanceof C);
