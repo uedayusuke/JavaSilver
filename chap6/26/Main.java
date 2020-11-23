be84bb33c49102c60e197ce46ea276882176a168
uedayusuke <uedayusuke1107@gmail.com>
2020-11-23 17:23:03 +0900

    [ADD]chap6:匿名クラス

--- chap6/26/Main.java
+++ chap6/26/Main.java
@@ -2,6 +2,7 @@ import java.util.function.Function;
 
 public class Main {
 	public static void main(String[] args) {
+		//クラス名を指定せずに、クラスの定義、メソッドの実装、オブジェクト生成を一つの式として直接記述
 		String str = new Function<String, String>() {
 			public String apply(String str) {
 				return "Hello " + str;
