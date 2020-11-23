59766450e1af2244b29ed31f3b20f4f312181253
uedayusuke <uedayusuke1107@gmail.com>
2020-11-23 16:00:14 +0900

    [ADD]chap6:Arraysクラスのメソッド

--- chap6/23/Main.java
+++ chap6/23/Main.java
@@ -6,12 +6,17 @@ public class Main {
 		print(ary1);
 		System.out.println();
 
+		//Arraysクラスのsortメソッドで昇順に並び替え
 		Arrays.sort(ary1);
 		print(ary1);
 		System.out.println();
 
 		Object[] ary2 = {"hello", 2};
-		//Arrays.sort(ary2);
+		/*
+		異なる型の要素を持つ配列にはsortメソッドは使えない
+		実行時にエラーが起きる
+		Arrays.sort(ary2);
+		*/
 	}
 
 	public static void print(int[] ary) {
