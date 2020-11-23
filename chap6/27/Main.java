8be5469c5cef3b1435bce7f864f58e1feb676589
uedayusuke <uedayusuke1107@gmail.com>
2020-11-23 18:06:23 +0900

    [ADD]chap6:ラムダ式

--- chap6/27/Main.java
+++ chap6/27/Main.java
@@ -2,6 +2,10 @@ import java.util.function.Function;
 
 public class Main {
 	public static void main(String[] args) {
+		/*
+		ラムダ式：
+		(実装するメソッドの引数) -> {処理};
+		*/
 		Function<String, String> obj = (String str) -> {
 			return "Hello " + str;
 		};
