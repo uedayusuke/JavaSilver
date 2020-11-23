7e370362a64f6e2de0be1527b72322e488c645dd
uedayusuke <uedayusuke1107@gmail.com>
2020-11-23 23:53:29 +0900

    [ADD]chap7:例外の発生

--- chap7/1/Main.java
+++ chap7/1/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
+		//int型の配列に初期値を代入
 		int[] num = {10, 20, 30};
+
+		//配列の中身を出力　→ 要素外にアクセスする為例外(エラー)が発生　→ 強制終了　
 		for (int i = 0; i < 4; i++ ) {
 			System.out.println("num :" + num[i]);
 			System.out.println(" : " + (i + 1) + "回目のループ");
