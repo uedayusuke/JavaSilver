fc0579c4ca6a24e5179e23533ade9d24e6cd933a
uedayusuke <uedayusuke1107@gmail.com>
2020-11-24 00:22:43 +0900

    [ADD]chap7:try-catch構文

--- chap7/2/Main.java
+++ chap7/2/Main.java
@@ -2,14 +2,18 @@ public class Main {
 	public static void main(String[] args) {
 		int[] num = {10, 20, 30};
 		for (int i = 0; i < 4; i++) {
+			//try：例外が発生しそうな場所
 			try{
 				System.out.println("num :" + num[i]);
 				System.out.println(" : " + (i + 1) + "回目のループ");
-			} catch(ArrayIndexOutOfBoundsException e) {
+			}
+			//catch：例外が発生した時の処理を定義　catch (例外クラス名　変数名) {}
+			catch(ArrayIndexOutOfBoundsException e) {
 				System.out.println("例外が発生しました。");
 			}
 		}
 
+		//強制終了することなく下記も処理される
 		System.out.println("---- end ----");
 	}
 }
