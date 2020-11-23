0fcc4e603da6544f97a2b1a9c256322f5665034f
uedayusuke <uedayusuke1107@gmail.com>
2020-11-23 17:20:29 +0900

    [ADD]chap6:匿名クラス

---
+++ chap6/26/Main.java
@@ -0,0 +1,13 @@
+import java.util.function.Function;
+
+public class Main {
+	public static void main(String[] args) {
+		String str = new Function<String, String>() {
+			public String apply(String str) {
+				return "Hello " + str;
+			}
+		}.apply("yusuke");
+
+		System.out.println(str);
+	}
+}
