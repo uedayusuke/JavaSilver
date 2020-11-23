85cf0a677f9cea608993645e55940ed1ebd72889
uedayusuke <uedayusuke1107@gmail.com>
2020-11-23 18:25:01 +0900

    [ADD]chap6:ラムダ式の変数

--- chap6/28.1/Main.java
+++ chap6/28.1/Main.java
@@ -4,6 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		int i = 25;
 		Supplier<Integer> s = () -> i;
+		/*
+		ラムダ式から参照されるローカル変数はfinal、
+		または事実上のfinal変数である為値を変更することはできない
+		i++
+		コンパイルエラーになる
+		*/
 		System.out.println(s.get());
 	}
 }
