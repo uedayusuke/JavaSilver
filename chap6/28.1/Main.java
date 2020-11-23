5d03c6b1312df5c2dd1cb24c866aaf836f5a595e
uedayusuke <uedayusuke1107@gmail.com>
2020-11-23 18:22:37 +0900

    [ADD]chap6:ラムダ式の変数

---
+++ chap6/28.1/Main.java
@@ -0,0 +1,9 @@
+import java.util.function.Supplier;
+
+public class Main {
+	public static void main(String[] args) {
+		int i = 25;
+		Supplier<Integer> s = () -> i;
+		System.out.println(s.get());
+	}
+}
