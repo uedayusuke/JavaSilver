68ac07e4e896a8455d9355dc1034cfc1c6d5034f
uedayusuke <uedayusuke1107@gmail.com>
2020-11-25 00:44:02 +0900

    [ADD]chap8:他モジュールの利用

--- chap8/2/com.seshop.sample/Main.java
+++ chap8/2/com.seshop.sample/Main.java
@@ -1,11 +1,13 @@
 package com.seshop.sample.main;
 //下記でFirstクラスをインポート
 import com.seshop.sample.firstpkg.First;
+import com.seshop.sample.secondpkg.Second;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Module System!");
 		//Firstクラスのxメソッドを呼び出し
 		First.x();
+		Second.y();
 	}
 }
