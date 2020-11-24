ceb229f9c3d801cce3fe9465a9e534da4b48c759
uedayusuke <uedayusuke1107@gmail.com>
2020-11-24 23:46:43 +0900

    [ADD]chap8:モジュール宣言とクラス格納

---
+++ chap8/1/src/Main.java
@@ -0,0 +1,8 @@
+//モジュールないのクラスはパッケージ日する必要がある為、パッケージ化してモジュール内に格納
+package com.seshop.sample.main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Module System!");
+	}
+}
