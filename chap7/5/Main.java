b1941288234c120861030ee12aad1db8e6bbd730
uedayusuke <uedayusuke1107@gmail.com>
2020-11-24 10:37:41 +0900

    [ADD]chap7:例外処理

--- chap7/5/Main.java
+++ chap7/5/Main.java
@@ -2,8 +2,10 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) {
+		//例外処理は任意
 		int i = Integer.parseInt("10");
 
+		//例外処理は必須
 		FileReader r = new FileReader("Test.txt");
 	}
 }
